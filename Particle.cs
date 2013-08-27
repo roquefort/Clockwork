346a0c60839ac2c172aa8f7e3a0adf9907f58a10
Danny Wicks <dannyjae91@gmail.com>
2013-08-27 21:43:12 +1000

    Base Physics engine

---
+++ Particle.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Laws of motions
+ * 
+ * 1. An object continues with a constant velocity unless a force acts upon it
+ * 2. A force acting on an object produces acceleration that is proportional to the objects mass
+ * */
+
+namespace Clockwork
+{
+    class Particle
+    {
+        //linear position in world space
+        Vector3 position;
+
+        //linear velocity in world space
+        Vector3 velocity;
+
+        //acceleration of the particle, can
+        //be used to set acceleration due to
+        //gravity. Its primary use or any other
+        //constant acceleration
+        Vector3 acceleration;
+
+        //holds the amount of damping applied
+        //to linear motion, damping is required
+        //to remove energy added through
+        //numerical instability in the integrator
+        double damping;
+
+        //more useful to hold the inverse mass
+        //because integration is simpler and because in real
+        //time sumulation it is easier to have objects with
+        //infinite mass(immovable) then objects with zero mass
+        //completely unstable in numerical situations
+        double inverseMass;
+
+        Vector3 forceAccum;
+
+        /*integrates the particle forward in time by the given amount
+         * uses newton-euler integration method, which is a linear 
+         * approximation of the correct integral. may be innacurate in some cases 
+         * */
+        void Integrate(float duration)
+        {
+            if (duration > 0.0)
+            {
+                //update linear position
+                position.AddScaledVector(velocity, duration);
+
+                //work out the accelertation from the force
+                //Vector3 forceAccum = new Vector3(0,0,0);
+                Vector3 resultingAcc = acceleration;
+                resultingAcc.AddScaledVector(forceAccum, inverseMass);
+
+                //update linear velocity from the acceleration
+                velocity.AddScaledVector(resultingAcc, duration);
+
+                //impose drag
+                //remove if wanting to simulate more particles
+                velocity *= Math.Pow(damping, duration);
+            }
+        }
+    }
+}
---
+++ Vector3.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clockwork
+{
+    public class Vector3
+    {
+        public double x;
+        public double y;
+        public double z;
+
+        public Vector3()
+        {
+            this.x = 0;
+            this.y = 0;
+            this.z = 0;
+        }
+
+        public Vector3(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        //inverts the vector
+        public void Invert()
+        {
+            x = -x;
+            y = -y;
+            z = -z;
+        }
+
+        //gets the magnitude of this vector
+        public double Magnitude()
+        {
+            return Math.Sqrt(x * x + y * y + z * z);
+        }
+
+        //gets the square magnitude of this vector
+        public double SquareMagnitude()
+        {
+            return (x * x + y * y + z * z);
+        }
+
+        //multiply vector by scalar
+        /*
+        public void Multiply(double scale)
+        {
+            x *= scale;
+            y *= scale;
+            z *= scale;
+        }
+        */  
+         
+        //multiply by scalar product
+        public static Vector3 operator * (Vector3 vec3, double value)
+        {
+            vec3.x *= value;
+            vec3.y *= value;
+            vec3.z *= value;
+            return vec3;
+        }
+
+        
+        //scalar product of 2 vectors
+        public static double operator * (Vector3 vec3a, Vector3 vec3b)
+        {
+            return vec3a.x * vec3b.x + vec3a.y * vec3b.y + vec3a.z * vec3b.z;
+        }
+
+        //adds two vectors
+        public static Vector3 operator + (Vector3 vec3a, Vector3 vec3b)
+        {
+            vec3a.x += vec3b.x;
+            vec3a.y += vec3b.y;
+            vec3a.z += vec3b.z;
+            return vec3a;
+        }
+
+        //subtracts two vectors
+        public static Vector3 operator - (Vector3 vec3a, Vector3 vec3b)
+        {
+            vec3a.x -= vec3b.x;
+            vec3a.y -= vec3b.y;
+            vec3a.z -= vec3b.z;
+            return vec3a;
+        }
+
+        public static Vector3 operator %(Vector3 vec3a, Vector3 vec3b)
+        {
+            return new Vector3(vec3a.y * vec3b.z - vec3a.z * vec3b.y,
+                vec3a.z * vec3b.x - vec3a.x * vec3b.z,
+                vec3a.x * vec3b.y - vec3a.y * vec3b.x);
+        }
+
+        //adds a vector scaled by amount
+        public void AddScaledVector(Vector3 vec3, double scale)
+        {
+            x += vec3.x * scale;
+            y += vec3.y * scale;
+            z += vec3.z * scale;
+        }
+
+        //component product (the least useful, no real application)
+        public void ComponentProduct(Vector3 vec3)
+        {
+            x *= vec3.x;
+            y *= vec3.y;
+            z *= vec3.z;
+        }
+
+        //calculates and returns the vector product of 
+        //this vector with the given vector
+        public Vector3 VectorProduct(Vector3 vec3)
+        {
+            return new Vector3(y * vec3.z - z * vec3.y, 
+                z * vec3.x - x * vec3.z,
+                x * vec3.y - y * vec3.x);
+        }
+        
+        /*
+        scalar product, same as * but long handed version
+        scalar product is faster then vector
+        It relates the scalar product to the length
+        of the two vectors and the angle between them
+         * */
+        public double ScalarProduct(Vector3 vec3)
+        {
+            return x * vec3.x + y * vec3.y + z * vec3.z;
+        }
+
+        /*
+        public static Vector3 Normalize(ref Vector3 vec3)
+        {
+            double mag = vec3.Magnitude();
+            if (mag > 0)
+                return vec3 *= ((double)1) / mag;
+            return vec3;
+
+        }
+         **/
+
+        /*
+        public static Vector3 Normalize(this Vector3 vec3)
+        {
+            double mag = vec3.Magnitude();
+            if (mag > 0)
+                return vec3 *= ((double)1) / mag;
+            return 0;
+        }
+         * */
+    }
+}
